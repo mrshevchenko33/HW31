e65c929d9646db3cc0871f56a1acd3a762950004
Ihor <mrshevchenko33@gmail.com>
2024-04-07 21:57:17 +0300

    Initial commit

---
+++ group.py
@@ -0,0 +1,33 @@
+
+class Group:
+    MAX_CAPACITY = 10
+
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        if len(self.group) >= self.MAX_CAPACITY:
+            raise GroupOverflowError
+        else:
+            self.group.add(student)
+
+    def delete_student(self, last_name):
+        student_to_remove = self.find_student(last_name)
+        if student_to_remove:
+            self.group.remove(student_to_remove)
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def __str__(self):
+        all_students = '\n'.join(str(student) for student in self.group)
+        return f'Number:{self.number}\n{all_students}'
+
+class GroupOverflowError(Exception):
+    def __init__(self, message="Group capacity exceeded"):
+        self.message = message
+        super().__init__(self.message)
